58e8ea15584bfe46b77ef19adc1ea8c04ec9ff38
claudiahW <murungiclaud@gmail.com>
2024-07-19 19:35:51 +0300

    add an event listener to prevent the page from refreshing

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@ import "./styles.css";
 import { useState } from "react";
 
 export default function App() {
-  const [] = useState("");
+  const [newItem, setNewItem] = useState("");
+
+  function handleSubmit(e){
+    e.preventDefault()
+  }
 
   return (
     <>
-      <form className="new-item-form">
+      <form onSubmit={handleSubmit}  className="new-item-form">
         <div className="form-row">
           <label htmlFor="">New Item</label>
-          <input type="text" id="item" />
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
+          />
         </div>
         <button className="btn">Add</button>
       </form>
