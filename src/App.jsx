cd721b865ebc1ef5530bd37ebe0ea69407e89e83
claudiahW <murungiclaud@gmail.com>
2024-07-19 19:18:49 +0300

    add a state to implement interactivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [] = useState("");
+
   return (
-    <form className="new-item-form">
-      <div className="form-row">
-        <label htmlFor="">New Item</label>
-        <input type="text" id="item" />
-      </div>
-      <button className="btn">Add</button>
-    </form>
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="">New Item</label>
+          <input type="text" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 2
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
   );
 }
