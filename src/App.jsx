2e63e2075ef433d5870b354cd855acf9379b82b8
claudiahW <murungiclaud@gmail.com>
2024-07-19 18:53:45 +0300

    add a button to add new items to the todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import "./styles.css"
+import "./styles.css";
 
-export default function App(){
- return <form className="new-item-form">
-
- </form>
+export default function App() {
+  return (
+    <form className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="">New Item</label>
+        <input type="text" id="item" />
+      </div>
+      <button className="btn">Add</button>
+    </form>
+  );
 }
