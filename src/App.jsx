8e1887b0ecddfe4379671ddb7175fe7eacffe86f
claudiahW <murungiclaud@gmail.com>
2024-07-19 19:58:01 +0300

    add a functionality to input data dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,22 @@ import { useState } from "react";
 
 export default function App() {
   const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
 
-  function handleSubmit(e){
-    e.preventDefault()
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ];
+    });
   }
 
   return (
     <>
-      <form onSubmit={handleSubmit}  className="new-item-form">
+      <form onSubmit={handleSubmit} className="new-item-form">
         <div className="form-row">
           <label htmlFor="">New Item</label>
           <input
@@ -24,20 +32,17 @@ export default function App() {
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 1
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
-        <li>
-          <label>
-            <input type="checkbox" />
-            Item 2
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
+        {todos.map((todo) => {
+          return (
+            <li>
+              <label>
+                <input type="checkbox" checked={todo.completed} />
+               {todo.title}
+              </label>
+              <button className="btn btn-danger">Delete</button>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
